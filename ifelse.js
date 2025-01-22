5510f998283f120cee89e490b9ba5329011a1a17
ManaswibRane <manaswirane1947@gmail.com>
2025-01-23 03:55:45 +0530

    Javascript

---
+++ ifelse.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+let a=1;
+for(var i=0; i<10 ;i++){
+    i+=a;
+    console.log(i);
+
+}
+if(a<18){
+    console.log("You Cannot Drive");
+}
+else {
+    console.log("Youcan drive");
+}
+let obj={
+    name: "Harry",
+    role: "Programmer"
+}
+for (const key in obj) {
+    const element=obj[key];
+        console.log(key,"--->",element);
+    }
