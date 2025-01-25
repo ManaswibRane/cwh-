4dca8a47fd9c3a7c8729730fc2627eea81384463
ManaswibRane <manaswirane1947@gmail.com>
2025-01-25 08:45:24 +0530

    Hello

---
+++ string.js
@@ -0,0 +1,14 @@
+let a="harry"
+console.log(a[4])
+console.log(a.length)
+let name=" World";
+console.log(`His name is ${a}`)
+console.log(a.toUpperCase())
+console.log('Roh"an')
+console.log(a.slice(1,4))
+console.log(a.replace("r",66))
+console.log(a.concat(a,"Hello","whdeik"))
+console.log(a.charAt(1))
+console.log(a.startsWith("h"))
+console.log(a.endsWith("h"))
+console.log(a.indexOf("rr"))
---
+++ variablesanddatatypes.js
@@ -0,0 +1,18 @@
+var a=5
+var c="Harry"
+console.log(typeof a , typeof c)
+//   var---->globally scoped 
+//  const , let ----->block scoped
+let x="Hello"
+let y=22;
+let z=3.55;
+const p=true;
+let q=undefined;
+let r=null;
+console.log(x,y,z,p,q,r);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+let o={
+    name:"Harry",
+    "job code ":500,
+}
+console.log(o)
