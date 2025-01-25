7b8d361ab53efb277dc9b2184352304a66de9ccb
ManaswibRane <manaswirane1947@gmail.com>
2025-01-25 10:29:09 +0530

    DOM added

---
+++ documentmodel/dom.js
@@ -0,0 +1,6 @@
+document.title="Harry is good"
+console.log(document.body)
+document.body.style.backgroundColor="green"
+console.log(window)
+console.log(window.document)
+console.log(window.document.body)
