294c4f5ef946b79a2c27c04b5b448441ffe5f674
ManaswibRane <manaswirane1947@gmail.com>
2025-01-25 07:53:12 +0530

    Hello

---
+++ faultycal.js
@@ -0,0 +1,11 @@
+a=Math.random()
+const func1=(x)=>{
+    if(a<=0.5){
+           console.log("Less than half")
+    }
+    else{
+        console.log("Greater than half")
+    }
+}
+func1(2);
+console.log(typeof a)
--- functions.js
+++ functions.js
@@ -1,4 +1,13 @@
 function nice(name){
-    console.log("Hey"+name+" how are you")
+    console.log("Hey "+name+" how are you")
 }
 nice("Harry")
+function sum(a,b){
+    return a+b;
+}
+res=sum(1,2)
+console.log(res)
+const func1=(x)=>{
+    console.log(x)
+}
+func1(15)
